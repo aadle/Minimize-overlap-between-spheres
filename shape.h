014e180e1e923784893acff8b2977b89f534fbcf
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-11-05 12:15:19 +0100

    get_position() method now functional

--- shape.h
+++ shape.h
@@ -6,13 +6,16 @@
 class Shape {
     public:
         // get
-        float* get_position() const {return this->coordinates;}
+        const float* get_position() const {return this->coordinates;} // return as const as we don't want the positional coordinates we return out to be able to change
+        float get_volume(); // General volume method which we can overload for different shapes.
 
-        // set
-        void set_coordinates(float* position[3]) {this->coordinates;}
+        // set 
+        void set_coordinates(float* position) {
+            for(int i=0; i < sizeof(position); i++) {this->coordinates[i] = position[i];}
+        } // perhaps not very general and doesn't look pretty with the for-loop but it works.
 
     private:
-        float coordinates[3] = {0.0, 0.0, 0.0};
+        float coordinates[3] {0.0, 0.0, 0.0};
         // std::vector<float> coordinates{0.0, 0.0, 0.0}; Don't use vector, use an array as the size doesn't vary. It's always 3 in size (x, y, z)
 };
 
