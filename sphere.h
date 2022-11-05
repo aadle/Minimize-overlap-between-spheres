6addfbdee7ef7ca6774d964fa78114d723cf6708
Aaron De Leyos <aaron.leyds.mangaron.de.leyos@nmbu.no>
2022-11-05 12:16:02 +0100

    get_ methods now working. Doesn't work calculating in private.

--- sphere.h
+++ sphere.h
@@ -1,19 +1,20 @@
 #ifndef SPHERE
 #define SPHERE
 
+#include <cmath>
 #include "shape.h"
 
-class Sphere {
+class Sphere: public Shape {
     public:
         // get
-        float get_diameter() const {return this->diameter;}
+        float get_diameter() const {return 2 * this->radius;}
         float get_radius() const {return this->radius;}
+        float get_volume() const {return 4 * M_PI * this->radius * this->radius;}
 
         // set
         void set_radius(float r) {this->radius = r;}
 
     private:
-        float diameter = 2 * radius;
         float radius = 0.0;
 };
 
